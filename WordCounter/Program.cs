b544ff9339d4a8613c1f3df2becae187180136a4
Ethan Firpo <suzannedeakins@Ethan-Firpo-CyberDeck.local>
2020-05-15 13:03:59 -0700

    add final branching of choices within app, add more ascii art

--- WordCounter/Program.cs
+++ WordCounter/Program.cs
@@ -18,27 +18,58 @@ namespace WordApp
       Console.WriteLine("     \\/  \\/ \\___/|_|  \\__,_|  \\_____\\___/ \\__,_|_| |_|\\__\\___|_|    /_/    \\_\\ .__/| .__/ ");
       Console.WriteLine("                                                                             | |   | |");
       Console.WriteLine("                                                                             |_|   |_| ");
-      Console.WriteLine("\nHere at Word Counter App we check a sentence to see how many times a certain word appears in it. Neat, huh? \nWould you like to try it out? [ Y/ N ]");
+      Console.WriteLine("\nHere at Word Counter App we believe in high-level design and functionality adapted to our fast paced modern world.\n\nWould you like to check a sentence to see how many times a certain word appears in it? [ Y / N ]");
       string tryInput = Console.ReadLine().ToLower();
       if (tryInput == "y")
       {
+        Console.WriteLine("\nWe knew you were the clever sort.");
       GetWordInput:
-        Console.WriteLine("Please enter a word below:");
+        Console.WriteLine("\nPlease enter a word below:\n");
         WordCounterApp.GetWord(Console.ReadLine());
         bool checkWord = WordCounterApp.CheckWord();
         if (checkWord == false)
         {
-          Console.WriteLine("You must have typoed. Please enter a word that contains only alphabetical characters.");
+          Console.WriteLine("\nYou must have typoed. Please enter a word that contains only alphabetical characters.");
           goto GetWordInput;
         }
         else
         {
-          Console.WriteLine("Please enter a sentence to check the word against:");
+          Console.WriteLine("\nPlease enter a sentence to check the word against:\n");
           WordCounterApp.GetSentence(Console.ReadLine());
           int wordCount = WordCounterApp.CheckSentence();
-          Console.WriteLine("Your sentence contains the word \"" + WordCounterApp.Word + "\" " + wordCount + " times.");
+          Console.WriteLine("\nYour sentence contains the word \"" + WordCounterApp.Word + "\" " + wordCount + " times. Wow!\n");
+          Console.WriteLine("Would you like to try again? [ Y / N ]\n");
+          string tryAgain = Console.ReadLine().ToLower();
+          if (tryAgain == "y")
+          {
+            goto GetWordInput;
+          }
+          else if (tryAgain == "n")
+          {
+            Console.WriteLine("\nThank you for trying out our next-level program.\n");
+            Console.WriteLine("                        _ _                _ ");
+            Console.WriteLine("                       | | |              | |");
+            Console.WriteLine("   __ _  ___   ___   __| | |__  _   _  ___| |");
+            Console.WriteLine("  / _` |/ _ \\ / _ \\ / _` | '_ \\| | | |/ _ \\ |");
+            Console.WriteLine(" | (_| | (_) | (_) | (_| | |_) | |_| |  __/_|");
+            Console.WriteLine("  \\__, |\\___/ \\___/ \\__,_|_.__/ \\__, |\\___(_)");
+            Console.WriteLine("   __/ |                         __/ |");
+            Console.WriteLine("  |___/                         |___/ \n");
+          }
         }
 
+      }
+      else if (tryInput == "n")
+      {
+        Console.WriteLine("\nWe understand. Not everyone can grasp the insane level\nof functionality we actualize here at Word Counter App. \nPlease come back another time if you find yourself \nready to get your mind blown.\n");
+        Console.WriteLine("                        _ _                _ ");
+        Console.WriteLine("                       | | |              | |");
+        Console.WriteLine("   __ _  ___   ___   __| | |__  _   _  ___| |");
+        Console.WriteLine("  / _` |/ _ \\ / _ \\ / _` | '_ \\| | | |/ _ \\ |");
+        Console.WriteLine(" | (_| | (_) | (_) | (_| | |_) | |_| |  __/_|");
+        Console.WriteLine("  \\__, |\\___/ \\___/ \\__,_|_.__/ \\__, |\\___(_)");
+        Console.WriteLine("   __/ |                         __/ |");
+        Console.WriteLine("  |___/                         |___/ \n");
       }
     }
   }
