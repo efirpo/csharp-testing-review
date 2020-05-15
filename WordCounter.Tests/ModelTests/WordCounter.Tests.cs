5f7efec3b8168fb814c08e773993d70e1d06cba6
Ethan Firpo <suzannedeakins@Ethan-Firpo-CyberDeck.local>
2020-05-15 10:12:09 -0700

    add fail test for checking if sentence is a valid string

--- WordCounter.Tests/ModelTests/WordCounter.Tests.cs
+++ WordCounter.Tests/ModelTests/WordCounter.Tests.cs
@@ -8,7 +8,6 @@ namespace WordCounter.Tests
   public class WordTests
   {
     [TestMethod]
-
     public void Word_CheckIfWordInputIsValidString_String()
     {
       // Act
@@ -17,7 +16,16 @@ namespace WordCounter.Tests
       WordCounterApp.GetWord("chuck");
       // Assert
       Assert.AreEqual(typeof(string), WordCounterApp.Word.GetType());
-
+    }
+    [TestMethod]
+    public void Sentence_CheckIfSentenceIsValidString_String()
+    {
+      // Act
+      WordCounterApp word = new WordCounterApp();
+      // Arrange
+      WordCounterApp.GetSentence("How much wood would a woodchuck chuck if a woodchuck could chuck wood?");
+      // Assert
+      Assert.AreEqual(typeof(string), WordCounterApp.Sentence.GetType());
     }
 
   }
