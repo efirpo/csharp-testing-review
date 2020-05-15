ee5f1e29e8a80f477a972bc0fe1433322e8d4a43
Ethan Firpo <suzannedeakins@Ethan-Firpo-CyberDeck.local>
2020-05-15 12:38:50 -0700

    add ascii art to UI

--- WordCounter/Program.cs
+++ WordCounter/Program.cs
@@ -9,7 +9,16 @@ namespace WordApp
     public static void Main()
     {
       WordCounterApp word = new WordCounterApp();
-      Console.WriteLine("Welcome to the Word Counter App!\n Here at Word Counter App we check a sentence to see how many times a certain word appears in it. Neat, huh? \nWould you like to try it out? [ Y/ N ]");
+      Console.WriteLine("\n                   Welcome to the");
+      Console.WriteLine(" __          __           _    _____                  _");
+      Console.WriteLine(" \\ \\        / /          | |  / ____|                | |                /\\");
+      Console.WriteLine("  \\ \\  /\\  / /__  _ __ __| | | |     ___  _   _ _ __ | |_ ___ _ __     /  \\   _ __  _ __");
+      Console.WriteLine("   \\ \\/  \\/ / _ \\| '__/ _` | | |    / _ \\| | | | '_ \\| __/ _ \\ '__|   / /\\ \\ | '_ \\| '_ \\ ");
+      Console.WriteLine("    \\  /\\  / (_) | | | (_| | | |___| (_) | |_| | | | | ||  __/ |     / ____ \\| |_) | |_) |");
+      Console.WriteLine("     \\/  \\/ \\___/|_|  \\__,_|  \\_____\\___/ \\__,_|_| |_|\\__\\___|_|    /_/    \\_\\ .__/| .__/ ");
+      Console.WriteLine("                                                                             | |   | |");
+      Console.WriteLine("                                                                             |_|   |_| ");
+      Console.WriteLine("\nHere at Word Counter App we check a sentence to see how many times a certain word appears in it. Neat, huh? \nWould you like to try it out? [ Y/ N ]");
       string tryInput = Console.ReadLine().ToLower();
       if (tryInput == "y")
       {
