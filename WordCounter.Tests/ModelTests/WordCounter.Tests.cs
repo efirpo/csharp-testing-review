40a0087d9885af62f5db7bbc375ab952af4778ea
Ethan Firpo <suzannedeakins@Ethan-Firpo-CyberDeck.local>
2020-05-15 10:02:45 -0700

    add fail test to check if input is string

--- WordCounter.Tests/ModelTests/WordCounter.Tests.cs
+++ WordCounter.Tests/ModelTests/WordCounter.Tests.cs
@@ -1,11 +1,24 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using WordCounter.Models;
+using System;
 
 namespace WordCounter.Tests
 {
   [TestClass]
   public class WordTests
   {
+    [TestMethod]
+
+    public void Word_CheckIfWordInputIsValidString_String()
+    {
+      // Act
+      WordCounterApp word = new WordCounterApp();
+      // Arrange
+      WordCounterApp.GetWord("chuck");
+      // Assert
+      Assert.AreEqual(typeof(string), WordCounterApp.Word.GetType());
+
+    }
 
   }
 }
